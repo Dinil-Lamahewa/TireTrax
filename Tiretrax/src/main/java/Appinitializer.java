1841b8df9fee28e696a056d0fc5cf7e4d27d1f7c
Dinil Lamahewa <dinillamahewa00@gmail.com>
2024-07-30 11:53:32 +0530

    Initial Commit

--- Tiretrax/src/main/java/Appinitializer.java
+++ Tiretrax/src/main/java/Appinitializer.java
@@ -1,11 +1,10 @@
-package Org.Freelancers;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class AppInitializer extends Application {
+public class Appinitializer extends Application {
+
     public static void main(String[] args) {
         launch(args);
     }
--- Tiretrax/src/main/java/Org/Freelancers/Controller/HomeController.java
+++
@@ -1,4 +0,0 @@
-package Org.Freelancers.Controller;
-
-public class HomeController {
-}
