afd2c4340ded35caa7b601c04fad7c5c11176da5
Yuwani Ranasinghe <yuwani@agrithmics.lk>
2024-07-30 23:18:38 +0530

    Create Customer Stock and SystemUsers Dtos

---
+++ Tiretrax/src/main/java/dto/SystemUsers.java
@@ -0,0 +1,20 @@
+package entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Data
+public class SystemUsers {
+    private String UserId ;
+    private String UserName ;
+    private String Password ;
+    private String Email ;
+    private String JobRole ;
+    private Date LastLoginDate ;
+}
