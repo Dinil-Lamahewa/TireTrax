03ee0ab48a045e16953fc8f0a88ea8b394ef3f50
Yuwani Ranasinghe <yuwani@agrithmics.lk>
2024-07-30 22:58:50 +0530

    Created Admins Customer FOrm Sample Page and it's Controller

--- Tiretrax/src/main/java/controller/AddCustomerController.java
+++ Tiretrax/src/main/java/controller/AddCustomerController.java
@@ -1,4 +1,36 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class AddCustomerController {
+    public TextField txtFirstName;
+    public TextField txtLastName;
+    public TextField txtAddress;
+    public TextField txtContact;
+    public TextField txtEmail;
+    public TextField txtCreditLimit;
+    public TextField txtCreditPeriod;
+    public ComboBox cmbCustomerType;
+    public Button btnBack;
+    public Button btnSave;
+    public Button btnUpdate;
+    public TableView tblCustomer;
+    public TableColumn cusId;
+    public TableColumn firstName;
+    public TableColumn lastName;
+    public TableColumn contact;
+    public TableColumn email;
+    public TableColumn type;
+    public TableColumn creditPeriod;
+    public TableColumn creditLimit;
+
+    public void btnBackOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateOnAction(ActionEvent actionEvent) {
+    }
 }
