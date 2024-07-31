926455b04ba3dd4f611c84620574f15cfcd09a18
Dinil Lamahewa <dinillamahewa00@gmail.com>
2024-07-31 08:00:09 +0530

    update AppInitializer class

--- Tiretrax/src/main/java/AppInitializer.java
+++ Tiretrax/src/main/java/AppInitializer.java
@@ -3,6 +3,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class AppInitializer extends Application {
 
     public static void main(String[] args) {
@@ -10,7 +12,7 @@ public class AppInitializer extends Application {
     }
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../resources/View/Home.fxml"))));
+        stage.setScene(new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("View/Home.fxml")))));
         stage.centerOnScreen();
         stage.setTitle("TireTrax");
         stage.setResizable(false);
