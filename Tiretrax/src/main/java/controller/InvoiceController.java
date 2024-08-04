9c79a609b2b582983c350aeb0036ab9c52d68c0f
Dinil Lamahewa <dinillamahewa00@gmail.com>
2024-08-04 08:53:27 +0530

    update invoice form and login form password field

--- Tiretrax/src/main/java/controller/InvoiceController.java
+++ Tiretrax/src/main/java/controller/InvoiceController.java
@@ -1,4 +1,32 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
 public class InvoiceController {
+    public Label InvoId;
+    public DatePicker DateAndTimeId;
+    public TextArea RemraksId;
+    public TextField GrossAmountId;
+    public TextField vatRateId;
+    public TextField NetAmountId;
+    public ComboBox CustomerId;
+    public ComboBox ItemId;
+    public TableView CartTblId;
+    public TableColumn ItemNameTblId;
+    public TableColumn QtyTblId;
+    public TableColumn UnitPriceTblId;
+    public TableColumn TotalPriceTblId;
+    public Button PrintId;
+    public Button ReturnPoId;
+    public CheckBox ReturnId;
+
+    public void PrintIdbtnOnAction(ActionEvent actionEvent) {
+    }
+
+    public void ReturnPOOnAction(ActionEvent actionEvent) {
+    }
+
+    public void ReturnIdOnAction(ActionEvent actionEvent) {
+    }
 }
